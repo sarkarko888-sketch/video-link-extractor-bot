37fb51121a37a0d1984c2e2c284cd5abe27c3888
sarkarko888-sketch <sarkarko888@gmail.com>
2025-12-04 21:15:30 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-if [ ! -f ".env" ]; then
-    echo "⚠ ERROR: .env file missing!"
-    echo "Create .env file with:"
-    echo "BOT_TOKEN=YOUR_TELEGRAM_BOT_TOKEN"
-    exit 1
-fi
-
-echo "🚀 Starting bot..."
+echo "🤖 Starting Bot..."
 node bot.js
